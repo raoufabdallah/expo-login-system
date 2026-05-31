cb585d9fbbb8994528c886b79c7537d665286859
Raouf Abdallah <raoufabdallah0909@gmail.com>
2026-05-31 21:38:17 +0300

    gitignore fix

---
+++ backend/secret.py
@@ -0,0 +1,1 @@
+link = "postgresql://neondb_owner:npg_yC2vuXdLQ6TB@ep-curly-shadow-almokwc3-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.db import engine, Base
 from backend.routers.signup import router as signup_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(signup_router)
